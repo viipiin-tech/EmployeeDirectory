a1afc94eb92d41377d9080aac2fbd092a3edd40f
vipin <vipin.passion1986@gmail.com>
2018-08-15 18:58:03 +0530

    changes

--- src/app/employee-info/employee-info.component.ts
+++ src/app/employee-info/employee-info.component.ts
@@ -1,8 +1,8 @@
-import { EmployeeService } from '../employee.service';
-import { Component, OnInit } from '@angular/core';
-import { IEmployee } from './employee';
-import { FormControl, FormGroup, FormBuilder, NgForm, Validators  } from '@angular/forms';
-import { UUID } from 'angular2-uuid';
+import {EmployeeService} from '../employee.service';
+import {Component, OnInit} from '@angular/core';
+import {IEmployee} from './employee';
+import {FormControl, FormGroup, FormBuilder, NgForm, Validators} from '@angular/forms';
+import {UUID} from 'angular2-uuid';
 
 @Component({
   selector: 'app-employee-info',
@@ -10,130 +10,138 @@ import { UUID } from 'angular2-uuid';
   styleUrls: ['./employee-info.component.css']
 })
 export class EmployeeInfoComponent implements OnInit {
-currentRow: IEmployee;
+  currentRow: IEmployee;
   isDisabled: boolean = true;
   employeeForm: FormGroup;
   modeValue: any;
-  employeeList:IEmployee[] = new Array();
-  
-    
-  
+  employeeList: IEmployee[] = new Array();
+  errorMsg:string;
 
-  constructor(private fb:FormBuilder,  private employeeService: EmployeeService) { }
+
+
+
+  constructor(private fb: FormBuilder, private employeeService: EmployeeService) {}
 
   ngOnInit() {
     this.getEmployee();
-    
-    this.employeeForm = this.fb.group ({
-      _id:[''],
-       name: ['', Validators.required],
-        email: ['',
-    [Validators.required, 
-    Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-        dob: ['', Validators.required],
-        department: ['', Validators.required],
-        gender: ['', Validators.required],
-        age: [''],
-      
+
+    this.employeeForm = this.fb.group({
+      _id: [''],
+      name: ['', Validators.required],
+      email: ['',
+        [Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      dob: ['', Validators.required],
+      department: ['', Validators.required],
+      gender: ['', Validators.required],
+      age: [''],
+
     })
 
   }
 
-    get email() { return this.employeeForm.get('email'); }
-     
-    get name() { return this.employeeForm.get('name'); }
- 
-    get gender() { return this.employeeForm.get('gender'); }
- 
-    get age() { return this.employeeForm.get('age'); }
-  
-  get dob() { return this.employeeForm.get('dob'); }
- 
-    get department() { return this.employeeForm.get('department'); }
-  
+  get email() {return this.employeeForm.get('email');}
+
+  get name() {return this.employeeForm.get('name');}
+
+  get gender() {return this.employeeForm.get('gender');}
+
+  get age() {return this.employeeForm.get('age');}
+
+  get dob() {return this.employeeForm.get('dob');}
+
+  get department() {return this.employeeForm.get('department');}
+
   public onFormSubmit() {
-        if(this.employeeForm.valid) {
-          this.currentRow = this.employeeForm.value;
-          this.CalculateAge();
-          console.log(this.currentRow);
-
-            /* Any API call logic via services goes here */
-          this.save();
-          
-          this.employeeList.push(this.currentRow);
-          this.currentRow = new IEmployee();
-           console.log("Form Submitted!");
-         this.employeeForm.reset();
-        }
+    if (this.employeeForm.valid) {
+      this.currentRow = this.employeeForm.value;
+      if(this.CalculateAge()){
+        this.save();
+        this.employeeList.push(this.currentRow);
+        this.employeeForm.reset();
+      }
     }
-  
+  }
 
-  
-   public CalculateAge(): void
-     {
-         if(this.currentRow.dob){
-            
-         let dob:any = new Date(this.currentRow.dob).getTime();
-            let timeDiff : any = Date.now() - dob;
-            //Used Math.floor instead of Math.ceil
-            //so 26 years and 140 days would be considered as 26, not 27.
-            this.currentRow.age = Math.abs(new Date(timeDiff).getUTCFullYear()-1970);
-           this.employeeForm.setValue(this.currentRow);
-        }
+
+  public CalculateAge(): boolean {
+    if (this.currentRow.dob) {
+
+      let dob: any = new Date(this.currentRow.dob).getTime();
+      let timeDiff: any = Date.now() - dob;
+      if (timeDiff > 0) {
+      
+      this.currentRow.age = Math.abs(new Date(timeDiff).getUTCFullYear() - 1970);
+      this.employeeForm.setValue(this.currentRow);
+        return true;
+      }
+      else
+        {
+        this.errorMsg = 'DOB Should not have future date';
+        return false;
+      }
     }
-  
-  editEmployee = function (employee:IEmployee) {  
-    this.currentRow = Object.assign({}, employee);
-  }; 
-  
-  getTemplate = function (employee:IEmployee) {  
-    if (employee._id === this.selected._id){  
-        return 'edit';  
-    }  
-    else return 'display';  
-};
-  
-  
-   getEmployee() {
-    this.employeeService.getEmployeeHttp().subscribe((data) => { this.employeeList=data}
-                                                               ,(err) => console.log('Error', err));
-    
   }
-  
-   updateEmployee(employee:IEmployee):void{
-     console.log(employee);
-   
 
-   this.employeeService.updateEmployeeWithId("_id",employee._id).subscribe((users) => {
-                                                                                       this.employeeList = users
-                                                                                       this.getEmployee();
-                                                                                      },
-                                                                        ((err) => console.log('Error', err)));
-    
-  }
-  
-  removeEmployee(employee:IEmployee):void{
-      console.log(employee);
+  editEmployee = function(employee: IEmployee) {
+this.errorMsg = '';
+    console.log(employee);
+    this.employeeForm = this.fb.group({
+      _id: [employee._id],
+      name: [employee.name],
+      email: [employee.email],
+      dob: [employee.dob],
+      department: [employee.department],
+      gender: [employee.gender],
+      age: [employee.age],
 
-   this.employeeService.deleteEmployeeWithId("_id",employee._id).subscribe((users) => {
-                                                                                       this.employeeList = users
-                                                                                       this.getEmployee();
-                                                                                      },
-                                                                        ((err) => console.log('Error', err)));
-    
+    })
+  };
+
+  
+
+
+  getEmployee() {
+    this.employeeService.getEmployeeHttp().subscribe((data) => {
+    this.employeeList = data
+      console.log(data)
+    },
+      (err) => console.log('Error', err));
+
   }
-  
-   
-  
-   save():void { 
-     this.modeValue = 'add';
-   
-     console.log(this.currentRow);
-   
-  //  this.employee = new employee:IEmployee('', this.employeeForm.value.name, this.employeeForm.value.email, this.employeeForm.value.dob, this.employeeForm.department, this.employeeForm.value.gender,, this.employeeForm.value.age);
-    this.employeeService.postEmployee(this.currentRow).subscribe((data) => {console.log('Success', data), this.currentRow=data}
+
+  updateEmployee(employee: IEmployee): void {
+    console.log(employee);
+    if(this.CalculateAge()){
+   this.employeeService.updateEmployeeWithId(employee).subscribe((users) => {
+      this.employeeList = users
+      this.getEmployee();
+    },
+      ((err) => console.log('Error', err)));
+    }
+  }
+
+  removeEmployee(employee: IEmployee): void {
+    console.log(employee);
+
+    this.employeeService.deleteEmployeeWithId("_id", employee._id).subscribe((users) => {
+      this.employeeList = users
+      this.getEmployee();
+    },
+      ((err) => console.log('Error', err)));
+
+  }
+
+
+
+  save(): void {
+    this.modeValue = 'add';
+
+    console.log(this.currentRow);
+
+   this.employeeService.postEmployee(this.currentRow).subscribe((data) => {console.log('Success', data), this.currentRow = data}
       , (err) => console.log('Error', err));
- console.log(this.currentRow);
+    console.log(this.currentRow);
   }
 
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,18 +2,12 @@ import {Injectable} from '@angular/core';
 import {IEmployee} from './employee-info/employee';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
-
-
 import {catchError, retry} from 'rxjs/operators';
-
-import { Http, Response, Headers, RequestOptions, URLSearchParams } 
-from '@angular/http';
-import { Observable } from 'rxjs';
-
+import {Http, Response, Headers, RequestOptions, URLSearchParams} from '@angular/http';
+import {Observable} from 'rxjs';
 // Observable class extensions
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
-
 // Observable operators
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
@@ -23,68 +17,70 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 
-
 @Injectable()
 export class EmployeeService {
-
- headers: Headers;
-    options: RequestOptions;
-
-    constructor(private http: Http) {
-        this.headers = new Headers({ 'Content-Type': 'application/json', 
-                                     'Accept': 'q=0.8;application/json;q=0.9' });
-        this.options = new RequestOptions({ headers: this.headers });
-    }
-
-    postEmployee(user: IEmployee): Observable<any> {
+  headers: Headers;
+  options: RequestOptions;
+  
+  
+  constructor(private http: Http) {
+    this.headers = new Headers({
+      'Content-Type': 'application/json',
+      'Accept': 'q=0.8;application/json;q=0.9'
+    });
+    this.options = new RequestOptions({headers: this.headers});
+  }
+  
+  postEmployee(user: IEmployee): Observable<any>
+  {
     let body = JSON.stringify(user);
-      let postUrl = "https://young-springs-16530.herokuapp.com/add";
+    let postUrl = "https://young-springs-16530.herokuapp.com/add";
     return this.http
-        .post(postUrl, body, this.options)
-        .map(this.extractData)
-        .catch(this.handleError);
-    }   
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body || {};
-    }
-  
-    private getData(res: Response) {
-        let body = res._body;
-        return JSON.parse(body) || {};
-    }
-  
-    getEmployeeHttp(): Observable<IEmployee[]> {
-    let getUrl = "https://young-springs-16530.herokuapp.com/employee";
-    return this.http
-        .get(getUrl, this.options)
-        .map(this.getData)
-        .catch(this.handleError);
-    }  
-  
-  deleteEmployeeWithId(key: string, val: string): Observable<IEmployee[]> {
-     let deleteUrl = "https://young-springs-16530.herokuapp.com/delete";
-    return this.http
-      .delete(deleteUrl + "/" + val, this.options).map(this.extractData).catch(this.handleError)
-    ;
-  }
-  
-  updateEmployeeWithId(key: string, val: string): Observable<any> {
-     let updateUrl = "https://young-springs-16530.herokuapp.com/update"+ "/" + val;
-    return this.http
-      .delete(updateUrl, this.options).pipe(retry(3),
-      catchError(this.handleError)
-    );
-  }
-  
-
-    private handleError(error: any) {
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        console.error(errMsg);
-        return Observable.throw(errMsg);
-    }
+      .post(postUrl, body, this.options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }  
+   
+   
+private extractData(res: Response) {
+  let body = res.json();
+  return body || {};
 }
-
-
+  
+  
+private getData(res: Response) {
+  let body = res._body;
+  return JSON.parse(body) || {};
+}
+  
+  
+getEmployeeHttp(): Observable < IEmployee[] > {
+  let getUrl = "https://young-springs-16530.herokuapp.com/employee";
+  return this.http
+    .get(getUrl, this.options)
+    .map(this.getData)
+    .catch(this.handleError);
+}
+  
+  
+deleteEmployeeWithId(key: string, val: string): Observable < IEmployee[] > {
+  let deleteUrl = "https://young-springs-16530.herokuapp.com/delete";
+  return this.http
+    .delete(deleteUrl + "/" + val, this.options).map(this.extractData).catch(this.handleError);
+}
+  
+updateEmployeeWithId(user: IEmployee): Observable < IEmployee[] > {
+  let body = JSON.stringify(user);
+  let updateUrl = "https://young-springs-16530.herokuapp.com/update" + "/" + user._id;
+  return this.http
+    .put(updateUrl, body, this.options).map(this.extractData).catch(this.handleError);
+}
+  
+  
+private handleError(error: any) {
+  let errMsg = (error.message) ? error.message :
+    error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+  console.error(errMsg);
+  return Observable.throw(errMsg);
+}
+}
